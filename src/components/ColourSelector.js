86eb856876e6feacb023fd6b4d1925c01671f387
Vinay846 <Vinayvin84629@gmail.com>
2020-12-02 01:01:33 +0530

    ans updated

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
+  const { config, selectNextBackground } = props;
   const { background } = config;
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <>
+    <button className={props.config.className} onClick={() => selectNextBackground({background: background})}>
+      {props.config.label}
     </button>
-  )
+    </>
+  );
 }
 export default ColourSelector;
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,14 @@
+import React, {useState} from 'react';
+import '../styles/Child.css';
 
+export default function Selection(props) {
+    const [background, setBackground] = useState("");
+    const handleclick=()=>{
+        setBackground(props.applyColor().background)
+    }
+    return(
+        <div onClick={()=>handleclick()} className="fix-box" style={{backgroundColor: background}}>
+            <h2 className="subheading">Selection</h2>
+        </div>
+    );
+}
