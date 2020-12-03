9ae0bc13d789bf2908201bef50df302ee01ff497
Vinay846 <31594212+Vinay846@users.noreply.github.com>
2020-12-03 20:33:24 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,12 +1,17 @@
-import React, {useState} from 'react';
-import '../styles/Child.css';
+import React, { useState } from "react";
+import "./../styles/Child.css";
 
-export default function Selection(props) {
-    const [background, setBackground] = useState({background: ""});
-    console.log(background);
-    return(
-        <div onClick={()=>props.applyColor(setBackground)} className="fix-box" style={{backgroundColor: background.background}}>
-            <h2 className="subheading">Selection</h2>
-        </div>
-    );
-}
+const Selection = (props) => {
+  const [nextBackground, selectNextBackground] = useState({ background: "" });
+
+  return (
+    <div
+      style={{ background: nextBackground.background }}
+      className={"fix-box"}
+      onClick={() => props.applyColor(selectNextBackground)}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+export default Selection;
